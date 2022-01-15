a1502cd9667a31d6f7734fac59da8ee9fe98381b
RenatoArenas <renato.arenas2012@gmail.com>
2022-01-15 10:29:36 -0500

    El juego se puede iniciar con un botón y colocar los caracteres de forma intercalada

--- js/michi.js
+++ js/michi.js
@@ -1,2 +1,45 @@
-let player = "X";
-let playerTwo = "O";
+const playerOne = "X";
+const playerTwo = "O";
+let playerInf = "X";
+const bloque = document.getElementsByClassName("michi-table--block");
+const info = document.getElementById("michi-info");
+
+let c = 0;
+
+function ejecutarJuego() {
+    for (let i = 0; i < 9; i++) {
+        bloque[i].setAttribute('onclick', 'colocarCaracter(this)');
+    }
+    info.textContent = "Tu turno: X"
+}
+
+function intercalarCaracter(p1, p2) {
+    let player;
+    c++;
+    console.log(c);
+    if (c%2 == 0) {
+        player = p2;
+        playerInf = p1;
+    }
+    else {
+        player = p1;
+        playerInf = p2;
+    }
+
+    if (c < 9) {
+        info.textContent = "Tu turno: " + playerInf;
+    }
+    else {
+        info.textContent = "Juego terminado"
+    }
+    console.log(player);
+    return player;
+}
+
+function colocarCaracter(i) {
+    let caracter = intercalarCaracter(playerOne, playerTwo);
+    let bloqueS = bloque[i.id];
+    bloqueS.textContent = caracter;
+    bloqueS.classList.remove("michi-table--active");
+    bloqueS.onclick = null;
+}
